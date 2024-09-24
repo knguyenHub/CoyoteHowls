92f4a5ff216a9f85f5cc39c1c7d903b87ee1d2e2
Aaliyah McCollum <aaliyahlynnette@outlook.com>
2024-09-24 04:48:03 -0700

    Centered Image

--- coyote_howls/src/components/pages/Home.jsx
+++ coyote_howls/src/components/pages/Home.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
+      <center>
       <h1>Welcome to </h1>
-      <img src="https://picsum.photos/400" alt="Random Image" hspace="50"/>
+      <img src="https://picsum.photos/400/300" alt="Random Image" hspace="50"/>
       <h1>Coyote Howls</h1>
+      </center>
     </div>
   )
 }
