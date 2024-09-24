067f04061244e56fb4890489c08a457ecb81d63c
Aaliyah McCollum <aaliyahlynnette@outlook.com>
2024-09-24 04:19:53 -0700

    Added Images & Some Text
    Added images and some text to AboutUs &Home.jsx files

--- coyote_howls/src/components/pages/About.jsx
+++ coyote_howls/src/components/pages/About.jsx
@@ -4,43 +4,17 @@ export const About = () => {
   return (
     <div>
       <div>
-        Lorem ipsum dolor sit amet 
-        consectetur adipisicing elit. Cum 
-        similique corporis a. Nihil mollitia 
-        quos quasi. Magni odit quas eligendi 
-        illo facilis ipsum rem soluta nemo 
-        eius similique. Dolor dolores saepe, 
-        amet atque iste sequi placeat iure ea
-        suscipit ipsum vero. Dolores voluptatem 
-        itaque quasi sit est, repudiandae corrupti 
-        alias quam, architecto animi exercitationem,
-        repellendus iste a at accusantium nam voluptates
-        facere nisi quae quo. Aliquam quam possimus porro
-        tempora nulla neque cum libero quae deserunt? Nihil
-        nam fuga eaque provident molestiae delectus 
-        suscipit aspernatur voluptatem quos atque quasi 
-        exercitationem impedit, quisquam officia tempora 
-        laboriosam facere dolor fugiat assumenda eos.
+        <h1>About Us</h1>
       </div>
       <br />
       <div>
-        Lorem ipsum dolor sit amet 
-        consectetur adipisicing elit. Cum 
-        similique corporis a. Nihil mollitia 
-        quos quasi. Magni odit quas eligendi 
-        illo facilis ipsum rem soluta nemo 
-        eius similique. Dolor dolores saepe, 
-        amet atque iste sequi placeat iure ea
-        suscipit ipsum vero. Dolores voluptatem 
-        itaque quasi sit est, repudiandae corrupti 
-        alias quam, architecto animi exercitationem,
-        repellendus iste a at accusantium nam voluptates
-        facere nisi quae quo. Aliquam quam possimus porro
-        tempora nulla neque cum libero quae deserunt? Nihil
-        nam fuga eaque provident molestiae delectus 
-        suscipit aspernatur voluptatem quos atque quasi 
-        exercitationem impedit, quisquam officia tempora 
-        laboriosam facere dolor fugiat assumenda eos.
+      <img src="https://picsum.photos/300" alt="Random Image" />
+      <p>Coyote Howls is a convenient office hours scheduling website for professors 
+        and students offers a streamlined way to manage appointments and ensure better 
+        communication. This platform allows students to easily view available time slots, 
+        book appointments, and receive reminders, reducing the hassle of coordinating meetings. 
+        Professors benefit from an organized schedule that automatically adjusts to their availability, 
+        minimizing double-booking and time conflicts.</p>
       </div>
       <br />
       <div>
--- coyote_howls/src/components/pages/Home.jsx
+++ coyote_howls/src/components/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <h1>Welcome to </h1>
+      <img src="https://picsum.photos/400" alt="Random Image" hspace="50"/>
+      <h1>Coyote Howls</h1>
     </div>
   )
 }
